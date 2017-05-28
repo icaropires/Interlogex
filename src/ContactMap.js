e419369d03dc2d37180baa9c68ecf227fa547719
Marcelo Ferreira <marcelohpf@gmail.com>
2017-05-28 20:30:18 -0300

    Add google maps component
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

---
+++ src/ContactMap.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import {withGoogleMap, GoogleMap,
+        Marker} from "react-google-maps";
+
+const GoogleMapContainer = withGoogleMap(props => (
+  <GoogleMap
+    defaultZoom={props.zoom}
+    defaultCenter={props.defaultPosition}
+  >
+    <Marker defaultPosition={props.defaultPosition}
+      title="Nossa loja"
+    />
+  </GoogleMap>
+));
+
+export default class ContactMap extends React.Component {
+  render(){
+    const defaultPosition = {lat: -15.799533, lng: -48.021679};
+    const zoom = 12;
+    return (
+      <GoogleMapContainer
+        containerElement={
+          <div style={{ height: "100%", minHeight: "400px" }} />
+        }
+        mapElement={
+          <div style={{ height: "100%", minHeight: "400px" }} />
+        }
+        zoom={zoom}
+        defaultPosition={defaultPosition}
+      />
+    );
+  }
+}
