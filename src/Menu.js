16f1e8811cf8dd11456e9d54254ac529d3b75d9d
Marcelo Ferreira <marcelohpf@gmail.com>
2017-06-02 19:33:14 -0300

    Small fixes in menu, links, and contact
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- src/Menu.js
+++ src/Menu.js
@@ -2,24 +2,22 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {Navigation} from 'react-mdl';
 
+// <Link to="/quemsomos" className={this.state.linkState[1]} onClick={this.handleClick.bind(this,1)}>Sobre nós</Link>
 export default class MenuInterlogex extends React.Component {
   constructor(props){
     super(props);
-    this.state = {'linkState': Array(4).fill(""), 'prev': 0};
-    this.handleClick = this.handleClick;
-    this.state.linkState[0]="selected";
+    this.state = {'activeItem': "/"};
   }
-  handleClick(e, j){
-    this.state.linkState[this.state.prev]="";
-    this.setState({prev: e});
-    this.state.linkState[e]="selected";
+  componentWillReceiveProps(){
+    this.state = { activeItem: window.location.pathname};
+    window.scroll(0,0);
   }
+
   render(){
     return (
         <Navigation>
-            <Link className={this.state.linkState[0]} onClick={this.handleClick.bind(this,0)} to="/">Home</Link>
-            <Link to="/quemsomos" className={this.state.linkState[1]} onClick={this.handleClick.bind(this,1)}>Sobre nós</Link>
-            <Link to="/contato" className={this.state.linkState[3]}onClick={this.handleClick.bind(this,3)}>Contatos</Link>
+            <Link className={this.state.activeItem === "/"?"selected":""} to="/">Home</Link>
+            <Link to="/contato" className={this.state.activeItem === "/contato"?"selected":""}>Contatos</Link>
             <Link to="" onClick={()=> {window.location.href="http://google.com/";}}>Acompanhamento de pedidos</Link>
         </Navigation>
     );
