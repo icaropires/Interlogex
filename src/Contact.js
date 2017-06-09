ac8c85a357e8853cce7bf988c5b0d379edc51722
Icaro <icaropiresdf@hotmail.com>
2017-06-09 15:14:02 -0300

    Add instagram to social medias
    Signed-off-by: Icaro <icaropiresdf@hotmail.com>
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- src/Contact.js
+++ src/Contact.js
@@ -142,13 +142,21 @@ export default class Contact extends React.Component {
               <Cell col={12}>
                 <i className="mdi mdi-facebook mdi-24px" style={{marginRight: '10px'}} />
                 <Tooltip label="Visite nossa página do Facebook" large><Link style={{textDecoration: "none", color: "inherit"}} target="_blank" to="http://facebook.com/interlogex">@interlogex</Link></Tooltip>
-              </Cell>
-              <Cell col={12}>
-                <Icon name="email" style={{marginRight: '10px'}} />
-                <Tooltip label="Envie-nos um email" large><Link style={{textDecoration: "none", color: "inherit"}} target='_top' to="mailto:interlogex@interlogex.com.br?Subject=Contato">interlogex@interlogex.com.br</Link></Tooltip>
+             </Cell>
 
-              </Cell>
-            </Cell>
+
+
+	     <Cell col={12}>
+                <i className="mdi mdi-instagram mdi-24px" style={{marginRight: '10px'}} />
+                <Tooltip label="Perfil no Instagram" large><Link style={{textDecoration: "none", color: "inherit"}} target="_blank" to="http://instagram.com/interlogex">@interlogex</Link></Tooltip>
+             </Cell>
+
+
+               <Cell col={12}>
+                 <Icon name="email" style={{marginRight: '10px'}} />
+                 <Tooltip label="Envie-nos um email" large><Link style={{textDecoration: "none", color: "inherit"}} target='_top' to="mailto:interlogex@interlogex.com.br?Subject=Contato">interlogex@interlogex.com.br</Link></Tooltip>
+               </Cell>
+             </Cell>
             <Cell col={12}><h1>Endereço</h1></Cell>
             <Cell col={12}>
             <Icon name="home" style={{marginRight: '10px'}}/>SHVP Rua 10 Chácara 177 Casa 08 Condomínio Alphaville - Vicente Pires - DF
