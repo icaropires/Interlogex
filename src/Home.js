573ae39bc309ce47c8ca2c1b5869344f3552d8bb
Marcelo Ferreira <marcelohpf@gmail.com>
2017-07-26 23:10:22 -0300

    Split the content in Home page
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Link, BrowserRouter} from 'react-router-dom';
 import Divider from 'material-ui/Divider';
+import Paper from 'material-ui/Paper';
+import {Tabs, Tab} from 'material-ui/Tabs';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import {List, ListItem, ListItemContent
@@ -32,19 +34,23 @@ class CardService extends React.Component {
     return (
       <Card>
         <CardHeader
-          title="Motoboy"
-          subtitle="Contrato exclusivo"
+          title={this.props.mainTitle}
+          subtitle={this.props.mainSubTitle}
+          actAsExpander={true}
+          showExpandableButton={true}
         />
         <CardMedia
-          overlay={<CardTitle title="Motoboy" subtitle="Contrato exclusivo" />}
-    >
-          <img src="https://blog.loggi.com/wp-content/uploads/2014/08/mensageiros3.jpg" alt="" />
+          overlay={<CardTitle
+                      title={this.props.overlayTitle}
+                      subtitle={this.props.overlaySubTitle} />}
+        >
+          <img src={this.props.image} alt="" />
         </CardMedia>
-        <CardText>
-          Contrate um de nossos motoboys de forma exclusiva! Recrute-o por 1 dia, 1 semana ou o tempo que necessitar!
+        <CardText expandable={true}>
+          {this.props.children}
         </CardText>
     <CardActions>
-      <FlatButton label="Action1" />
+      <Link to="/servicos"><FlatButton label="Veja mais!" /></Link>
     </CardActions>
   </Card>
     );
@@ -55,110 +61,141 @@ export default class Home extends React.Component {
   render() {
     return (
 <div>
-  <Slides arrowLeft={ArrowLeft}
-            arrowRight={ArrowRight}
-            dots={Dots}
-            duration={300}
-            delay={3000}
-            width="100%"
-            style={{marginLeft: "0px"}}>
-    <div className="slide-card">
-      <div className="slide-image">
-        <h2>Descubra nossos serviços</h2>
-        <img  src={services} />
+  <div className="divider">
+    <Slides arrowLeft={ArrowLeft}
+              arrowRight={ArrowRight}
+              dots={Dots}
+              duration={300}
+              delay={3000}
+              width="100%"
+              style={{marginLeft: "0px"}}>
+      <div className="slide-card">
+        <div className="slide-image">
+          <h2>Descubra nossos serviços</h2>
+          <img  src={services} />
+        </div>
+        <div className="slide-text">
+          Transportamos suas mercadorias com agilidade, compromisso, eficiência e rapidez em todo Distrito Federal e Goiás.
+        </div>
       </div>
-      <div className="slide-text">
-        Transportamos suas mercadorias com agilidade, compromisso, eficiência e rapidez em todo Distrito Federal e Goiás.
+      <div className="slide-card">
+        <div className="slide-image">
+          <h2>Relacionamento com os clientes</h2>
+          <img  src={negotiation} />
+        </div>
+        <div className="slide-text">
+          Nossos clientes são diversificados e, para atendermos cada vez melhor, buscamos sempre excelência em nossos serviços.
+        </div>
       </div>
-    </div>
-    <div className="slide-card">
-      <div className="slide-image">
-        <h2>Relacionamento com os clientes</h2>
-        <img  src={negotiation} />
+      <div className="slide-card">
+        <div className="slide-image">
+          <h2>Descubra quem somos</h2>
+          <img  src={business_values} />
+        </div>
+        <div className="slide-text">
+          Nós somos uma empresa de transporte de pequenos volumes e documentos sediada no Distrito Federal.
+        </div>
       </div>
-      <div className="slide-text">
-        Nossos clientes são diversificados e, para atendermos cada vez melhor, buscamos sempre excelência em nossos serviços.
+      <div className="slide-card">
+        <div className="slide-image">
+          <h2>Saiba como nós pensamos</h2>
+          <img  src={business} />
+        </div>
+        <div className="slide-text">
+          A nossa visão de negócio e que norteia nosso trabalho são baseados na nossa <strong>VISÃO</strong>, <strong>MISSÃO</strong> e <strong>VALORES</strong> 
+        </div>
       </div>
-    </div>
-    <div className="slide-card">
-      <div className="slide-image">
-        <h2>Descubra quem somos</h2>
-        <img  src={business_values} />
-      </div>
-      <div className="slide-text">
-        Nós somos uma empresa de transporte de pequenos volumes e documentos sediada no Distrito Federal.
-      </div>
-    </div>
-    <div className="slide-card">
-      <div className="slide-image">
-        <h2>Saiba como nós pensamos</h2>
-        <img  src={business} />
-      </div>
-      <div className="slide-text">
-        A nossa visão de negócio e que norteia nosso trabalho são baseados na nossa <strong>VISÃO</strong>, <strong>MISSÃO</strong> e <strong>VALORES</strong> 
-      </div>
-    </div>
-  </Slides>
+    </Slides>
+  </div>
   <Divider />
-  <h2>Serviços</h2>
-  <div className="card-wrapper">
-    <div className="card-item">
-      <CardService />
-    </div>
-    <div className="card-item">
+  <div className="divider">
+    <div className="home-title">Serviços</div>
+    <div className="card-wrapper">
+      <div className="card-item">
+        <CardService
+          mainTitle="Contratos"
+          overlayTitle="Entregadores"
+          mainSubTitle="Exclusividade"
+          image="https://blog.loggi.com/wp-content/uploads/2014/08/mensageiros3.jpg" >
+            Contrate um de nossos motoboys de forma exclusiva!
+            <Divider />
+            Recrute-o por 1 dia, 1 semana ou o tempo que necessitar!
+        </CardService>
+      </div>
+      <div className="card-item">
+        <CardService
+          mainTitle="Contratos"
+          overlayTitle="Entregadores"
+          mainSubTitle="Agendamento"
+          image="https://blog.loggi.com/wp-content/uploads/2014/08/mensageiros3.jpg" >
+            Faça o agendamento das entregas para períodos definidos por você!
+            <Divider />
+            Escolha um motoboy em específico e agende as entregas.
+        </CardService>
+      </div>
+      <div className="card-item">
+        <CardService
+          mainTitle="Sob demanda"
+          overlayTitle="Por horas"
+          mainSubTitle="Quando precisar"
+          image="https://blog.loggi.com/wp-content/uploads/2014/08/mensageiros3.jpg" >
+            Contrate um nossos serviços pela quantidade de horas que precisar!
+            <Divider />
+            Polpe seu dinheiro, pague somente as horas que necessitar das entregas.
+        </CardService>
+      </div>
+      <div className="card-item">
+        <CardService
+          mainTitle="Sob demanda"
+          overlayTitle="Pela distância"
+          mainSubTitle="Quando precisar"
+          image="https://blog.loggi.com/wp-content/uploads/2014/08/mensageiros3.jpg" >
+            Faça suas entregas e não se preocupe com o tempo.
+            <Divider />
+            Calculamos o valor de cada entrega baseado na distância da entrega.
+        </CardService>
+      </div>
     </div>
   </div>
   <Divider />
-        <CardHome image={services} title={"Nossos Serviços"}  link={{url: '/servicos', text: 'Veja mais serviços'}}>
-          <div>
-            Transportamos suas mercadorias com agilidade, compromisso, eficiência e rapidez em todo Distrito Federal e Goiás.
-            <br />
-            Vantagens em nos contratar:
-            <List style={{padding: '0'}}>
-              <ListItem style={{color: '#757575', padding:'0'}}>
-                <ListItemContent icon="location_on">Rastreamento da carga</ListItemContent>
-              </ListItem>
-              <ListItem style={{color: '#757575', padding:'0'}}>
-                <ListItemContent icon="assignment_turned_in">Confirmação de entrega</ListItemContent>
-              </ListItem>
-              <ListItem style={{color: '#757575', padding:'0'}}>
-                <ListItemContent icon="map">Coleta e entrega no local desejado</ListItemContent>
-              </ListItem>
-              <ListItem style={{color: '#757575', padding:'0'}}>
-                <ListItemContent icon="schedule">Coletas programadas com flexibilidade de horários</ListItemContent>
-              </ListItem>
-            </List>
-          </div>
-        </CardHome>
-
-        <CardHome image={negotiation} title={"Clientes"} link={{url: '/contato', text: 'Entre em contato'}}>
-          <div>
-            <p>Nossos clientes são diversificados e, para atendermos cada vez melhor, buscamos sempre excelência em nossos serviços.</p>
-            <p>Acreditamos que a melhor propaganda será a indicação dos nossos serviços.</p>
-            <p>Não abrimos mão de nossos valores e assim a cada dia construímos uma empresa que cumpre suas obrigações com os clientes, com os colaboradores e com a sociedade.</p>
-            <p>Para tanto, dispomos de uma equipe comprometida, eficiente, especializada e em busca constante pela excelência na qualidade dos serviços.</p>
-          </div>
-        </CardHome>
-
-        <CardHome image={business_values} title={"Um pouco sobre nós"} link={{url: '/quemsomos', text: 'Leia mais'}}>
+  <div className="divider">
+    <div className="home-title">Relacionamento com os clientes</div>
+    <div>
+      <Paper style={{marginLeft: '20%', height: "100px",width: '300px', textAlign: 'center'}} zDepth={4}>
+        <i>"Com a interlogex, eu conseguir ganhar tempo, pois não precisei esperar em filas de banco. O próprio entregador foi e resolveu pra mim" - <b>Cliente Anônimo</b></i>
+      </Paper>
+      <p>Nossos clientes são diversificados e, para atendermos cada vez melhor, buscamos sempre excelência em nossos serviços.</p>
+      <p>Acreditamos que a melhor propaganda será a indicação dos nossos serviços.</p>
+      <h2>Veja o que os clientes falam na página do <Link to="https://facebook.com/interlogex">Facebook</Link></h2>
+      <p>Não abrimos mão de nossos valores e assim a cada dia construímos uma empresa que cumpre suas obrigações com os clientes, com os colaboradores e com a sociedade.</p>
+      <p>Para tanto, dispomos de uma equipe comprometida, eficiente, especializada e em busca constante pela excelência na qualidade dos serviços.</p>
+    </div>
+  </div>
+  <Divider />
+  <div className="divider">
+      <div className="home-title">Um pouco sobre nós</div>
           <div>
             <p>Nós somos uma empresa de transporte de pequenos volumes e documentos sediada no Distrito Federal.</p>
             <p>Entregas rápidas e eficientes caracterizam nosso serviço, para isso contamos com uma equipe especializada, apta a prestar serviços em todo território nacional.</p>
             <p>Para isto, contamos com uma boa equipe de entregadores e excelentes em relacionamento com os clientes. Atendentes preparados para oferecer o melhor serviço a sua necessidade.</p>
           </div>
-        </CardHome>
-
-        <CardHome image={business} title={"O negócio"} link={{url: '/quemsomos', text: 'Leia mais'}}>
           <div>
             <p>A nossa visão de negócio e que norteia nosso trabalho são baseados na nossa <strong>VISÃO</strong>, <strong>MISSÃO</strong> e <strong>VALORES</strong> </p>
-            <p><strong>Valores: </strong>Trabalhamos de forma <strong>ética</strong>, buscamos a <strong>retidão</strong> na prestação de nossos serviços para nos mantermos na linha da <strong>justiça</strong>.</p>
-            <p><strong>Visão:</strong> Ser a melhor empresa de transportes para se trabalhar e de prestação de serviços.</p>
-            <p><strong>Missão:</strong> Oferecer serviços de transporte com eficiência, ética, qualidade e responsabilidade.</p>
+            <Tabs>
+              <Tab label="Valores">
+              <strong>Valores: </strong>
+              <p>Trabalhamos de forma <strong>ética</strong>, buscamos a <strong>retidão</strong> na prestação de nossos serviços para nos mantermos na linha da <strong>justiça</strong>.</p>
+              </Tab>
+              <Tab label="Visão">
+              <strong>Visão:</strong><p> Ser a melhor empresa de transportes para se trabalhar e de prestação de serviços.</p>
+              </Tab>
+              <Tab label="Missão">
+              <strong>Missão:</strong><p> Oferecer serviços de transporte com eficiência, ética, qualidade e responsabilidade.</p>
+              </Tab>
+            </Tabs>
           </div>
-        </CardHome>
-
-
-      </div>
+  </div>
+  </div>
     );
   }
 }
