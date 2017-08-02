722f7f08223c18938e094e3527529ff0e9973488
Icaro Pires <icaropiresdf@hotmail.com>
2017-08-02 18:49:55 -0300

    Add linkedin to SocialMedias and expand link to icons
    Signed-off-by: Icaro Pires <icaropiresdf@hotmail.com>

--- src/SocialMedias.js
+++ src/SocialMedias.js
@@ -4,38 +4,49 @@ import { Grid, Cell, Icon, Tooltip } from 'react-mdl';
 import { Link } from 'react-router-dom';
 
 export default class SocialMedias extends React.Component {
-     render(){
-         return(
-           <div>
-             <Cell col={12}><h1>Mídias sociais</h1></Cell>
+  render(){
+    return(
+      <div>
+        <Cell col={12}><h1>Mídias sociais</h1></Cell>
 
-               <Cell col={12}>
-                 <Cell col={12}>
-                   <i className="mdi mdi-facebook mdi-24px" style={{marginRight: '10px'}} />
-                   <Tooltip label="Visite nossa página do Facebook" large>
-                     <Link style={{textDecoration: "none", color: "inherit"}} target="_blank" to="http://facebook.com/interlogex">
-                       @interlogex
-                     </Link>
-                   </Tooltip>
-                 </Cell>
-                 <Cell col={12}>
-                   <i className="mdi mdi-instagram mdi-24px" style={{marginRight: '10px'}} />
-                   <Tooltip label="Perfil no Instagram" large>
-                     <Link style={{textDecoration: "none", color: "inherit"}} target="_blank" to="http://instagram.com/interlogex">
-                       @interlogex
-                     </Link>
-                   </Tooltip>
-                 </Cell>
-                 <Cell col={12}>
-                   <Icon name="email" style={{marginRight: '10px'}} />
-                   <Tooltip label="Envie-nos um email" large>
-                     <Link style={{textDecoration: "none", color: "inherit"}} target='_top' to="mailto:interlogex@interlogex.com.br?Subject=Contato">
-                       interlogex@interlogex.com.br
-                     </Link>
-                   </Tooltip>
-                 </Cell>
-             </Cell>
-           </div>
-        );
-    }
+        <Cell col={12}>
+          <Cell col={12}>
+            <Tooltip label="Visite nossa página do Facebook" large>
+              <Link style={{textDecoration: "none", color: "inherit"}} target="_blank" to="http://facebook.com/interlogex">
+                <Icon className="mdi mdi-facebook mdi-24px" style={{marginRight: '10px'}} />
+                @interlogex
+              </Link>
+            </Tooltip>
+          </Cell>
+
+          <Cell col={12}>
+            <Tooltip label="Perfil no Instagram" large>
+              <Link style={{textDecoration: "none", color: "inherit"}} target="_blank" to="http://instagram.com/interlogex">
+                <Icon className="mdi mdi-instagram mdi-24px" style={{marginRight: '10px'}} />
+                @interlogex
+              </Link>
+            </Tooltip>
+          </Cell>
+
+          <Cell col={12}>
+            <Tooltip label="Ir para perfil do Linkedin" large>
+              <Link style={{textDecoration: "none", color: "inherit"}} target="_blank" to="https://br.linkedin.com/in/internacional-interlogex">
+                <Icon className="mdi mdi-linkedin mdi-24px" style={{marginRight: '10px'}}/>
+                internacional-interlogex
+              </Link>
+            </Tooltip>
+          </Cell>
+
+          <Cell col={12}>
+            <Tooltip label="Envie-nos um email" large>
+              <Link style={{textDecoration: "none", color: "inherit"}} target='_top' to="mailto:interlogex@interlogex.com.br?Subject=Contato">
+                <Icon name="email" style={{marginRight: '10px'}} />
+                interlogex@interlogex.com.br
+              </Link>
+            </Tooltip>
+          </Cell>
+        </Cell>
+      </div>
+    );
+  }
 }
